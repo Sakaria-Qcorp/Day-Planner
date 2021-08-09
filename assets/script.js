a3cd23f578ddc1e7d305588f818200e2e39910ea
Zack <ahmedsakaria32@gmail.com>
2021-08-08 19:34:04 -0500

    set up else if statements for first textbox

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 
 var currentDate = moment().format("LLLL");
 //console.log(rightNow);
-var hourNow = moment().format("LT");
-//console.log(hourNow);
+var hourNow = moment().format("h");
+console.log(hourNow);
 var times = ["12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM", "12 pM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","11 PM"];
 
 $("#currentDay").text(currentDate);
@@ -31,10 +31,22 @@ var headingN = $("h2");
 // create a funtion showing the table of numbers
 
        
-var currentHour = date.getHours(); // Return index 0 through 23
-console.log(currentHour);
+
+
+
 
 // If/Else Statement - conditions for the hours
+if (hourNow > 7) {
+    // All textboxes will be green
+    var textBx = $("#first");
+    textBx.addClass("future");
+}else if (hourNow == 7){
+    var textBx = $("#first");
+    textBx.addClass("present");
+}else{
+    var textBx = $("#first");
+    textBx.addClass("past");
+}
 
 function LocalStorageSetUp() {
       
